3baf0deb67d9c1d69ccd6fe1ec7a7a65a64aa0a8
Morten Borup Petersen <morten_bp@live.dk>
2018-11-20 23:41:22 +0100

    Add runtime library functions for unsigned comparisons

---
+++ compiler-rt/lib/builtins/leros/ultsi3.c
@@ -0,0 +1,19 @@
+/** Morten Borup Petersen 20/11/2018
+ * Leros runtime library routine for unsigned comparison
+ */
+
+int __ultsi3(unsigned int ua, unsigned int ub) {
+  const int signMask = 0x80000000;
+  int a_s = ua & signMask;
+  int b_s = ub & signMask;
+
+  if (a_s ^ b_s) {
+    // Discrepancy in sign bit
+    // positive operand will always be the smaller of the two in an unsigned
+    // comparison Detect which operand was positive and return accordingly
+    return a_s > 0 ? 1 : 0;
+  }
+
+  // Equal sign for both operands, signed comparison can be used
+  return (int)ua < (int)ub;
+}
