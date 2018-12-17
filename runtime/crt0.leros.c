d4ef91694531fe62e5527d3c0f7d35839f9cca00
Morten Borup Petersen <morten_bp@live.dk>
2018-12-17 10:21:53 +0100

    Initialize ABI specified constant registers through _start

--- runtime/crt0.leros.c
+++ runtime/crt0.leros.c
@@ -5,7 +5,6 @@ asm("	start:		");
 
 __attribute__((naked))
 void _start(){    
-    // At some point we should initialize the C library here
 
     // Stack initialization - for now we just initialize the stack pointer at 0x7ffffff0
     asm("loadi      -16\n"
@@ -14,8 +13,42 @@ void _start(){
         "loadh3i    127\n"
         "store      r1\n");
 
-    // Kernel or host should have passed argc in r4 and argv in r5
+    // Constant register initialization
+    asm("loadi      0x0\n"
+        "store      r100\n"
 
+        "loadi      0x1\n"
+        "store      r101\n"
+
+        "loadi      0x80\n"
+        "loadhi     0x0\n"
+        "store      r102\n"
+
+        "loadi      0x0\n"
+        "loadhi     0x80\n"
+        "loadh2i    0x0\n"
+        "store      r103\n"
+
+        "loadi      0x0\n"
+        "loadh3i    0x80\n"
+        "store      r104\n"
+        
+        "loadi      0xFF\n"
+        "loadhi     0x0\n"
+        "store      r105\n"
+        
+        "loadhi     0xFF\n"
+        "loadh2i    0x0\n"
+        "store      r106\n"
+
+        "loadi      0x0\n"
+        "loadh2i    0xFF\n"
+        "store      r107\n"
+        );
+
+    // At some point we should initialize the C library here
+
+    // Kernel or host should have passed argc in r4 and argv in r5
     // Call main
     asm("loadi      main\n"
         "loadhi     main\n"
