f3bac8b84e89d3195baab159cbf5ee90d32436d1
Morten Borup Petersen <morten_bp@live.dk>
2019-08-09 18:26:05 +0200

    Add seven segment display addresses to Basys3 board header

--- cores/VelonaCore/basys3/VelonaCore_Basys3.h
+++ cores/VelonaCore/basys3/VelonaCore_Basys3.h
@@ -3,5 +3,13 @@
  */
 
 // Peripherals
-#define BASYS3_LED_ADDR    0x9FFF0000
-#define BASYS3_SW_ADDR     0x9FFF0004
+#define BASYS3_LED_ADDR     0x9FFF0000
+#define BASYS3_SW_ADDR      0x9FFF0004
+
+// Seven segment display
+#define BASYS3_7SD_BASE     0x9FFF0100U
+#define BASYS3_7SD_0        (BASYS3_7SD_BASE + 0x4)
+#define BASYS3_7SD_1        (BASYS3_7SD_BASE + 0x8)
+#define BASYS3_7SD_2        (BASYS3_7SD_BASE + 0xC)
+#define BASYS3_7SD_3        (BASYS3_7SD_BASE + 0x10)
+
